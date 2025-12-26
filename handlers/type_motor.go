359fb326a1742bf527c27032b8cdb3973db0ff21
YONATHAN\Admin <rifqi@kemakmuran.com>
2025-12-26 10:02:47 +0700

    change sql query

--- handlers/type_motor.go
+++ handlers/type_motor.go
@@ -13,6 +13,7 @@ func TypeMotorHandler(db *sql.DB) http.HandlerFunc {
 		rows, err := db.Query(`
 			SELECT TypeMotorDescription, COUNT(*) AS total
 			FROM sales_service_data
+			WHERE TypeMotorDescription IS NOT NULL
 			GROUP BY TypeMotorDescription
 			ORDER BY total DESC
 			LIMIT 10
